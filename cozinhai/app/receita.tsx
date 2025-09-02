a196688bb1c7a35ad8c5c2935abc2966edd3fdff
grazisouza1 <graziisouza0127@gmail.com>
2025-09-02 16:20:16 -0300

    Ajustando a posição do logo no componente Receita

--- cozinhai/app/receita.tsx
+++ cozinhai/app/receita.tsx
@@ -23,7 +23,7 @@ const styles = StyleSheet.create({
     resizeMode: "contain",
     justifyContent: "center",
     alignItems: "center",
-    top: 150,
+    top: 100,
   },
   main: {
     flex: 1,
