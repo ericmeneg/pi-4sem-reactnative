a6e4f721e9a64127a57722fab81bf93c1a47b705
ericmeneg <ericvinicius.menegon@gmail.com>
2025-09-04 15:42:44 -0300

    Adicionando componente DailyRecipes para uso na dashboard

---
+++ cozinhai/components/DailyRecipes.tsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react"
+import { IRecipe } from "../interfaces/recipe.interface"
+import { Text } from "react-native-paper"
+import RecipeCard from "./RecipeCard"
+
+function seededRand(seed: number){
+    let randomizedNumber = Math.sin(seed) * 1000
+    return randomizedNumber - Math.floor(randomizedNumber)
+}
+
+function getDailySeeds(): number[]{
+    let today = new Date().toISOString().split("T")[0]
+    let seed = parseInt(today.replace(/-/g, ""), 10)
+    const seeds: number[] = []
+
+    while (seeds.length < 3){
+        seed++
+        const random = (Math.floor(seededRand(seed)* 1000000))%5224
+        seeds.push(random)
+    }
+
+    return seeds
+}
+
+async function getDailyRecipes(offsets: number[]): Promise<IRecipe[]> {
+  const API_KEY = "d9e89aa107a2446ea222d9c3004ad5ed"
+  const baseUrl = "https://api.spoonacular.com/recipes/complexSearch"
+
+  const results = await Promise.all(
+    offsets.map(async (offset) => {
+      const res = await fetch(
+        `${baseUrl}?apiKey=${API_KEY}&number=1&offset=${offset}`
+      )
+      const data = await res.json()
+      return data.results?.[0] ?? null
+    })
+  )
+
+  return results.filter(Boolean) as IRecipe[]
+}
+
+export default function DailyRecipes() {
+
+    const [recipes, setRecipes] = useState<IRecipe[]>([])
+    useEffect(()=>{
+        async function loadRecipes() {
+            const seedArray = getDailySeeds()
+            const gotRecipes = await getDailyRecipes(seedArray)
+            setRecipes(gotRecipes)
+        } 
+        loadRecipes()
+    }, [])
+
+    return(
+        <>
+        {
+        recipes.map((recipe)=>(
+            <RecipeCard key={recipe.id} recipe={recipe} />
+        ))
+        }
+        </>
+    )
+}
