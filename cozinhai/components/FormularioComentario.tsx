7f6302d9919d0d43aff4f39872d66fe4ff265a8f
ericmeneg <ericvinicius.menegon@gmail.com>
2025-11-07 10:03:43 -0300

    feat: Criando a estrutura básica do componente FormularioComentario

---
+++ cozinhai/components/FormularioComentario.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { Pressable, View } from "react-native";
+import { Avatar, Card, Icon, Text, TextInput } from "react-native-paper";
+
+export default function FormularioComentario() {
+    //controlam o conteúdo dos campos do formulário
+    const [comentarioString, setComentarioString] = useState("")
+    const [foto, setFoto] = useState()
+    const [pontuacao, setPontuacao] = useState(0)
+
+    //controla a altura do text input do comentário
+    const [altura, setAltura] = useState(40)
+
+    return (
+        <View style={{ maxWidth: 275 }}>
+            <Card mode="contained" >
+                <Card.Content>
+                    <Card.Title titleStyle={{ fontSize: 14 }} title="Deixe sua opinião!" left={() => <Avatar.Icon icon="comment-plus-outline" size={48} style={{ backgroundColor: 'teal', marginLeft: -10 }} />} />
+                    <TextInput mode="outlined"
+                        activeOutlineColor="#22577A"
+                        label="Escreva um comentário"
+                        value={comentarioString}
+                        onChangeText={texto => setComentarioString(texto)}
+                        multiline
+                        onContentSizeChange={(e)=>{
+                            setAltura(e.nativeEvent.contentSize.height)
+                        }}
+                        style={{
+                            height: Math.min(altura, 200),
+                            textAlignVertical: "top"
+                        }} 
+                        />
+                    <Text style={{marginTop: 16}}>Dê uma pontuação a receita:</Text>
+                    <View style={{flexDirection: "row", justifyContent: "center"}}>
+                        {Array.from({ length: 5 }, (_, i) => (
+                        <Pressable onPress={()=>{setPontuacao(i+1)}}>
+                        <Icon
+                          key={i}
+                          source={i < pontuacao ? "star" : "star-outline"}
+                          size={30}
+                          color={i < pontuacao ? "#22577A" : "#CCC"}
+                        />
+                        </Pressable>
+                      ))}
+                    </View>
+                </Card.Content>
+            </Card>
+        </View>
+    )
+}
