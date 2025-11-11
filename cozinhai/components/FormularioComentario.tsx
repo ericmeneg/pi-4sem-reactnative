038b78bf8b97e22207a192efc1f1f481eea33f4f
ericmeneg <ericvinicius.menegon@gmail.com>
2025-11-11 10:26:17 -0300

    feat: adicionando visualização expandida da foto anexa ao comentário e opção de remover a foto via toque longo

--- cozinhai/components/FormularioComentario.tsx
+++ cozinhai/components/FormularioComentario.tsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import { Alert, Pressable, TouchableOpacity, View, Image } from "react-native";
-import { Avatar, Button, Card, Icon, Text, TextInput } from "react-native-paper";
+import { Avatar, Button, Card, Icon, Modal, Portal, Text, TextInput } from "react-native-paper";
 import * as ImagePicker from "expo-image-picker"
 
-async function pedirPermissao(){
+async function pedirPermissao() {
     const { status } = await ImagePicker.requestCameraPermissionsAsync()
     if (status !== 'granted') {
         Alert.alert("Permissão negada", "Precisamos de acesso a câmera para que você possa adicionar fotos ao seu comentário (fotos são opcionais)")
@@ -21,75 +21,104 @@ export default function FormularioComentario() {
     //controla a altura do text input do comentário
     const [altura, setAltura] = useState(40)
 
-    async function tirarFoto(){
-    const temPermissao = await pedirPermissao()
-    if (!temPermissao) return
+    //controla a visibilidade do modal de visualização da foto tirada
+    const [modalVisivel, setModalVisivel] = useState(false)
 
-    const result = await ImagePicker.launchCameraAsync({
-        base64: true,
-        quality: 0.7,
-        allowsEditing: true
-    })
+    async function tirarFoto() {
+        const temPermissao = await pedirPermissao()
+        if (!temPermissao) return
 
-    if (!result.canceled && result.assets.length > 0){
-        const foto = result.assets[0]
-        setFoto(`data:image/jpeg;base64,${foto.base64}`)
+        const result = await ImagePicker.launchCameraAsync({
+            base64: true,
+            quality: 0.7,
+            allowsEditing: true
+        })
+
+        if (!result.canceled && result.assets.length > 0) {
+            const foto = result.assets[0]
+            setFoto(`data:image/jpeg;base64,${foto.base64}`)
+        }
     }
-}
 
     return (
         <View style={{ maxWidth: 325 }}>
             <Card mode="contained" >
                 <Card.Content>
                     <Card.Title titleStyle={{ fontSize: 14 }} title="Deixe sua opinião!" left={() => <Avatar.Icon icon="comment-plus-outline" size={48} style={{ backgroundColor: 'teal', marginLeft: -10 }} />} />
-                    
+
                     <TextInput mode="outlined"
                         activeOutlineColor="#22577A"
                         label="Escreva um comentário"
                         value={comentarioString}
                         onChangeText={texto => setComentarioString(texto)}
                         multiline
-                        onContentSizeChange={(e)=>{
+                        onContentSizeChange={(e) => {
                             setAltura(e.nativeEvent.contentSize.height)
                         }}
                         style={{
                             height: Math.min(altura, 200),
                             textAlignVertical: "top"
-                        }} 
-                        />
-                    
-                    <Text style={{marginTop: 16}}>Dê uma pontuação a receita:</Text>
-                    <View style={{flexDirection: "row", justifyContent: "center"}}>
+                        }}
+                    />
+
+                    <Text style={{ marginTop: 16 }}>Dê uma pontuação a receita:</Text>
+                    <View style={{ flexDirection: "row", justifyContent: "center" }}>
                         {Array.from({ length: 5 }, (_, i) => (
-                        <Pressable onPress={()=>{setPontuacao(i+1)}}>
-                        <Icon
-                          key={i}
-                          source={i < pontuacao ? "star" : "star-outline"}
-                          size={30}
-                          color={i < pontuacao ? "#22577A" : "#CCC"}
-                        />
-                        </Pressable>
-                      ))}
+                            <Pressable onPress={() => { setPontuacao(i + 1) }}>
+                                <Icon
+                                    key={i}
+                                    source={i < pontuacao ? "star" : "star-outline"}
+                                    size={30}
+                                    color={i < pontuacao ? "#22577A" : "#CCC"}
+                                />
+                            </Pressable>
+                        ))}
                     </View>
 
-                    <Text style={{marginTop: 16}}>Adicione uma imagem do seu prato:</Text>
+                    <Text style={{ marginTop: 16 }}>Adicione uma imagem do seu prato:</Text>
                     <Button
-                    icon="camera"
-                    mode="contained-tonal" 
-                    onPress={tirarFoto} 
-                    style={{marginBottom: 10, backgroundColor: "#22557A"}}
-                    labelStyle={{color: "white"}} >
-                    Tirar Foto
+                        icon="camera"
+                        mode="contained-tonal"
+                        onPress={tirarFoto}
+                        style={{ marginBottom: 10, backgroundColor: "#22557A" }}
+                        labelStyle={{ color: "white" }} >
+                        Tirar Foto
                     </Button>
                     {foto && (
-                        <TouchableOpacity onPress={()=> console.log("Abrindo visualização ampliada")}>
-                            <Image
-                            source={{uri: foto}}
-                            style={{width:150, height: 150, borderRadius: 10, alignSelf: "center"}}
-                            />
-                        </TouchableOpacity>
+                        <>
+                            <TouchableOpacity
+                                onPress={() => setModalVisivel(true)}
+                                onLongPress={() => setFoto(null)}
+                                delayLongPress={1250}>
+                                <Image
+                                    source={{ uri: foto }}
+                                    style={{ width: 150, height: 150, borderRadius: 10, alignSelf: "center" }}
+                                />
+                            </TouchableOpacity>
+                            <Text style={{ color: "gray", alignSelf: "center" }}>Toque para expandir, segure para apagar</Text>
+                        </>
 
                     )}
+                    <Portal>
+                        <Modal
+                            visible={modalVisivel}
+                            onDismiss={() => setModalVisivel(false)}
+                            contentContainerStyle={{
+                                backgroundColor: "transparent",
+                                justifyContent: "center",
+                                padding: 0,
+                                alignItems: "center"
+                            }}
+                            style={{
+                                margin: 0,
+                                justifyContent: "center",
+                                alignItems: "center"
+                            }}>
+                            <Image
+                                source={{ uri: foto }}
+                                style={{ width: 400, height: 400, borderRadius: 15 }} />
+                        </Modal>
+                    </Portal>
                 </Card.Content>
             </Card>
         </View>
