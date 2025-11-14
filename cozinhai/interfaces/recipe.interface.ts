c6827ed14569a78950286b28a5db06cf532a2cd5
ericmeneg <ericvinicius.menegon@gmail.com>
2025-11-13 22:58:19 -0300

    Atualizando a interface de receitas para comportar informações importantes para o display de detalhes da receitas

--- cozinhai/interfaces/recipe.interface.ts
+++ cozinhai/interfaces/recipe.interface.ts
@@ -1,5 +1,11 @@
+import IIngredients from "./ingredient.interface"
+
 export interface IRecipe {
     id: number,
     title: string,
     image: string  
+    servings?: number
+    readyInMinutes?: number
+    extendedIngredients?: IIngredients[],
+    instructions?: string
 }
